f8d8b19a2ab2e11fdd37f2d2cadfbf099dcc62f7
runnerSnail <gaomengjie_forever@163.com>
2021-03-23 00:56:07 +0800

    update .gitignore

--- leetcode/gitInfoChange.sh
+++ leetcode/gitInfoChange.sh
@@ -5,12 +5,12 @@ an="$GIT_AUTHOR_NAME"
 am="$GIT_AUTHOR_EMAIL"
 cn="$GIT_COMMITTER_NAME"
 cm="$GIT_COMMITTER_EMAIL"
-if [ "$GIT_COMMITTER_EMAIL" = "要修改的@邮箱地址.com" ]
+if [ "$GIT_COMMITTER_EMAIL" = "gaoquankang@bytedance.com" ]
 then
     cn="runnerSnail"
     cm="gaomengjie_forever@163.com"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "要修改的@邮箱地址.com" ]
+if [ "$GIT_AUTHOR_EMAIL" = "gaoquankang@bytedance.com" ]
 then
     an="runnerSnail"
     am="gaomengjie_forever@163.com"
