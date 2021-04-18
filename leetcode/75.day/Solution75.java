1680c349b6c8fe7b3b0890dcebac78428bc2f0e7
runnerSnail <gaomengjie_forever@163.com>
2021-04-18 21:05:24 +0800

    update

--- leetcode/75.day/Solution75.java
+++ leetcode/75.day/Solution75.java
@@ -2,11 +2,22 @@ public class Solution75 {
     public void sortColors(int[] nums) {
         int ptr = 0;
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i]==0){
+            if (nums[i] == 0) {
                 int temp = nums[ptr];
-                nums[ptr] = temp;
+                nums[ptr] = nums[i];
+                nums[i] = temp;
+                ptr++;
             }
         }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                int temp = nums[ptr];
+                nums[ptr] = nums[i];
+                nums[i] = temp;
+                ptr++;
+            }
+        }
+
     }
 
     public static void main(String[] args) {
