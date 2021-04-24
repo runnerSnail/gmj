530f9c991d8140c3be380a2d55bdd16beb9c2e4b
runnerSnail <gaomengjie_forever@163.com>
2021-04-24 14:00:50 +0800

    update

---
+++ leetcode/91.day/Solution91.java
@@ -0,0 +1,21 @@
+public class Solution91 {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        // 1 1 10 1 1 1 0
+        for (int i = 1; i < dp.length; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + s.charAt(i) - '0' <= 26)) {
+                dp[i] += dp[i - 2];
+            }
+
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ leetcode/92.day/Solution92.java
@@ -0,0 +1,24 @@
+public class Solution92 {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
