5df6eec51c565bbdbb2803113b110eb07a4bfd67
runnerSnail <gaomengjie_forever@163.com>
2021-05-22 23:32:02 +0800

    update

--- leetcode/sword33/Sword33.java
+++ leetcode/sword33/Sword33.java
@@ -1,23 +1,34 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class Sword33 {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
+
         List<List<Integer>> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
+        int direction = 0;
         while (!queue.isEmpty()) {
             List<Integer> temp = new ArrayList<>();
             for (int i = queue.size(); i > 0; i--) {
                 TreeNode node = queue.poll();
                 temp.add(node.val);
-                if(node.left!=null) queue.add(node.left);
-                if(node.right!=null) queue.add(node.right);
+
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+
+                direction += 1;
+            }
+            if(direction%2==1){
+                Collections.reverse(temp);
             }
             list.add(temp);
         }
